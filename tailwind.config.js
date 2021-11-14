ad1d51ba964aaf8c7461f476842035aa1908d660
YOUR NAME <aetchevers@eagerworks.com>
2021-11-14 10:36:46 -0300

    Add configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "docs/*.html"
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -7,5 +9,7 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
 }
