b42437e258736e200ad9c8ee84dd228dae6b8ed1
YOUR NAME <aetchevers@eagerworks.com>
2021-11-23 14:57:45 -0300

    Added get for laboral experience

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
 var btnOpenModal = document.querySelectorAll('.modal-open')
 var btnCloseModal = document.querySelectorAll('.modal-close')
-const btnSubmit = document.getElementById("contacto_submit")
+var experienceBlock = document.getElementById('experiencia-laboral')
+const btnSubmit = document.getElementById('contacto_submit')
 const overlay = document.querySelector('.modal-overlay')
 const modal = document.querySelector('.modal')
 
-let inputNombre = document.getElementById("grid-nombre")
-let inputEmail = document.getElementById("grid-email")
-let inputTelefono = document.getElementById("grid-telefono")
-let inputMensaje = document.getElementById("grid-mensaje")
+let inputNombre = document.getElementById('grid-nombre')
+let inputEmail = document.getElementById('grid-email')
+let inputTelefono = document.getElementById('grid-telefono')
+let inputMensaje = document.getElementById('grid-mensaje')
 
 setup()
 
@@ -58,7 +59,7 @@ function sendContact() {
     }
     alert(`Gracias por tu contacto, ${contacto.nombre}. Me comunicaré pronto!`)
     console.log(contacto)
-    window.localStorage.setItem(`${contacto.email}`, JSON.stringify(contacto));
+    window.localStorage.setItem(`${contacto.email}`, JSON.stringify(contacto))
     clearModal()
   } else {
     inputRemoveWarning(inputNombre, inputEmail)
@@ -74,11 +75,11 @@ function clearModal() {
 }
 
 function errorMsg(name, email) {
-  if(name.value ==""){
+  if(name.value == ""){
       inputAddWarning(name)
   } 
 
-  if (email.value =="") {
+  if (email.value == "") {
       inputAddWarning(email)
   }
   alert(`Ingresa tu informacion de contacto!`)
@@ -97,3 +98,91 @@ function inputRemoveWarning(name, email) {
       email.classList.remove('border-red-500')
   }
 }
+
+function getLaboralExperience() {
+ fetch("http://pw2021-apinode-aleetchevers.alejandraetchev.repl.co/experiencia-laboral")
+  .then((response) => response.json()) 
+  .then((experiencias) => {
+    createLaboralExperienceSection(experiencias)
+  });
+}
+
+function createLaboralExperienceSection(experiencias) {
+  var div = document.createElement("div")
+  div.classList.add("text-gray-500")
+
+  experiencias['experiencia-laboral'].forEach(function (experiencia) {
+    var divExperiencia = document.createElement("div")
+    divExperiencia.classList.add("text-gray-500", "pt-6", "mt-6")
+  
+    var empresa = document.createElement("h3");
+    empresa.classList.add("text-blue-300", "font-bold")
+    empresa.textContent = experiencia.empresa;
+    divExperiencia.appendChild(empresa)
+
+    var section1 = document.createElement("section")
+    section1.classList.add("mb-6")
+
+    var fechas = document.createElement("div")
+    fechas.classList.add("lg:inline-block", "lg:w-3/12", "lg:align-top", "italic", "mb-2")
+    var fechaInicio = document.createElement("p")
+    fechaInicio.textContent = experiencia.fechaInicio
+    var fechaFin = document.createElement("p")
+    fechaFin.textContent = experiencia.fechaFin
+    
+    fechas.appendChild(fechaInicio)
+    fechas.appendChild(fechaFin)
+    section1.appendChild(fechas)
+
+    var puesto = document.createElement("div")
+    puesto.classList.add("lg:inline-block", "lg:w-8/12", "w-full")
+    puesto.textContent = experiencia.puesto
+    section1.appendChild(puesto)
+    
+    divExperiencia.appendChild(section1)
+
+    var section2 = document.createElement("section")
+    section2.classList.add("mb-6")
+
+    var acercaDe = document.createElement("div")
+    acercaDe.classList.add("lg:inline-block", "lg:w-3/12", "lg:align-top", "italic", "mb-2")
+    acercaDe.textContent = 'Acerca de : '
+    section2.appendChild(acercaDe)
+
+    var descripcion = document.createElement("div")
+    descripcion.classList.add("lg:inline-block", "lg:w-8/12", "w-full")
+    descripcion.textContent = experiencia.descripcion
+    section2.appendChild(descripcion)
+
+    divExperiencia.appendChild(section2)
+
+    if(experiencia.empresa == "EagerWorks") {
+      var section3 = document.createElement("section")
+      section3.classList.add("mb-6")
+
+      var divHerramientas = document.createElement("div")
+      divHerramientas.classList.add("lg:inline-block", "lg:w-3/12", "lg:align-top", "italic", "mb-2")
+      divHerramientas.textContent = 'Herramientas : '
+      section3.appendChild(divHerramientas)
+
+      var herramientas = document.createElement("div")
+      herramientas.classList.add("lg:inline-block", "lg:w-8/12", "w-full")
+      
+      let lista_herramientas = ['Rails', 'GitHub', 'Ruby', 'Bootstrap', 'Circle CI', 'Clickup|Trello', 'HTML|CSS']
+      for (var i = 0; i < lista_herramientas.length; i++) {
+        var span = document.createElement("span")
+        span.classList.add('rounded-xl', 'bg-blue-300', 'text-gray-500', 'px-3')
+        span.textContent = lista_herramientas[i]
+        herramientas.appendChild(span)
+      }
+      section3.appendChild(herramientas)
+      divExperiencia.appendChild(section3)
+    }
+    div.appendChild(divExperiencia)
+  })
+  experienceBlock.appendChild(div)
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  getLaboralExperience()
+})
